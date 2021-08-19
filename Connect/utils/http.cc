4d49885957dcbc1624a6154fef50b4d01a058369
Particle_G <particle_g@outlook.com>
2021-08-19 12:16:21 +0800

    - Add util class http

---
+++ Connect/utils/http.cc
@@ -0,0 +1,81 @@
+//
+// Created by Parti on 2021/2/4.
+//
+
+#include <utils/http.h>
+
+using namespace tech::utils;
+using namespace drogon;
+using namespace std;
+
+void http::fromJson(
+        const HttpStatusCode &code,
+        const Json::Value &body,
+        const function<void(const HttpResponsePtr &)> &callback
+) {
+    auto httpJsonResponse = HttpResponse::newHttpJsonResponse(body);
+    httpJsonResponse->setStatusCode(code);
+    callback(httpJsonResponse);
+}
+
+void http::fromJson(
+        const HttpStatusCode &code,
+        const Json::Value &body,
+        const vector<Cookie> &cookies,
+        const function<void(const HttpResponsePtr &)> &callback
+) {
+    auto httpJsonResponse = HttpResponse::newHttpJsonResponse(body);
+    httpJsonResponse->setStatusCode(code);
+    for (const auto &cookie : cookies) {
+        httpJsonResponse->addCookie(cookie);
+    }
+    callback(httpJsonResponse);
+}
+
+[[maybe_unused]] void http::fromPlain(
+        const HttpStatusCode &code, const string &body,
+        const function<void(const HttpResponsePtr &)> &callback
+) { from(code, CT_TEXT_PLAIN, body, callback); }
+
+[[maybe_unused]] void http::fromHtml(
+        const HttpStatusCode &code,
+        const string &body,
+        const function<void(const HttpResponsePtr &)> &callback
+) { from(code, CT_TEXT_HTML, body, callback); }
+
+void http::from(
+        const HttpStatusCode &code,
+        const ContentType &type,
+        const string &body,
+        const function<void(const HttpResponsePtr &)> &callback
+) {
+    auto httpResponse = HttpResponse::newHttpResponse();
+    httpResponse->setStatusCode(code);
+    httpResponse->setContentTypeCode(type);
+    httpResponse->setBody(body);
+    callback(httpResponse);
+}
+
+string http::toJson(
+        const HttpRequestPtr &req,
+        Json::Value &result
+) {
+    auto object = req->getJsonObject();
+    if (object) {
+        result = *object;
+        return req->getJsonError();
+    }
+    return "Invalid Json";
+}
+
+string http::toJson(
+        const HttpResponsePtr &req,
+        Json::Value &result
+        ) {
+    auto object = req->getJsonObject();
+    if (object) {
+        result = *object;
+        return req->getJsonError();
+    }
+    return "Invalid Json";
+}
---
+++ Connect/utils/http.h
@@ -0,0 +1,52 @@
+//
+// Created by Parti on 2021/2/4.
+//
+
+#pragma once
+
+#include <drogon/HttpController.h>
+#include <json/json.h>
+
+namespace tech::utils::http {
+    void fromJson(
+            const drogon::HttpStatusCode &code,
+            const Json::Value &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
+
+    void fromJson(
+            const drogon::HttpStatusCode &code,
+            const Json::Value &body,
+            const std::vector<drogon::Cookie> &cookies,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
+
+    [[maybe_unused]] void fromPlain(
+            const drogon::HttpStatusCode &code,
+            const std::string &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
+
+    [[maybe_unused]] void fromHtml(
+            const drogon::HttpStatusCode &code,
+            const std::string &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
+
+    void from(
+            const drogon::HttpStatusCode &code,
+            const drogon::ContentType &type,
+            const std::string &body,
+            const std::function<void(const drogon::HttpResponsePtr &)> &callback
+    );
+
+    std::string toJson(
+            const drogon::HttpRequestPtr &req,
+            Json::Value &result
+    );
+
+    std::string toJson(
+            const drogon::HttpResponsePtr &req,
+            Json::Value &result
+    );
+}
