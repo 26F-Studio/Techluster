4dfe925bcfc9996a7449e680f23a289c9e470c81
Particle_G <particle_g@outlook.com>
2021-09-06 04:43:51 +0800

    - Implement new monitoring method - Use timers to observe node changes

---
+++ Connect/plugins/NodeManager.cc
@@ -0,0 +1,109 @@
+//
+// Created by Particle_G on 2021/8/19.
+//
+
+#include <drogon/drogon.h>
+#include <plugins/NodeManager.h>
+
+using namespace drogon;
+using namespace std;
+using namespace tech::plugins;
+using namespace tech::structures;
+
+void NodeManager::initAndStart(const Json::Value &config) {
+    if (!(
+            config.isMember("nodeType") && config["nodeType"].isObject() &&
+            config["nodeType"].isMember("message") && config["nodeType"]["message"].isString() &&
+            config["nodeType"].isMember("gaming") && config["nodeType"]["gaming"].isString() &&
+            config["nodeType"].isMember("transfer") && config["nodeType"]["transfer"].isString() &&
+            config["nodeType"].isMember("user") && config["nodeType"]["user"].isString()
+    )) {
+        LOG_ERROR << R"(Invalid nodeType config)";
+        abort();
+    } else {
+        _typeMapper[config["nodeType"]["message"].asString()] = NodeServer::Type::message;
+        _typeMapper[config["nodeType"]["gaming"].asString()] = NodeServer::Type::gaming;
+        _typeMapper[config["nodeType"]["transfer"].asString()] = NodeServer::Type::transfer;
+        _typeMapper[config["nodeType"]["user"].asString()] = NodeServer::Type::user;
+    }
+
+    if (!(
+            config.isMember("heartBeat") && config["heartBeat"].isObject() &&
+            config["heartBeat"].isMember("waitTimes") && config["heartBeat"]["waitTimes"].isUInt()
+    )) {
+        LOG_ERROR << R"(Invalid heartBeat config)";
+        abort();
+    } else {
+        _waitTimes = config["heartBeat"]["waitTimes"].asUInt();
+    }
+
+    LOG_INFO << "NodeManager loaded.";
+}
+
+void NodeManager::shutdown() { LOG_INFO << "NodeManager shutdown."; }
+
+NodeServer::Type NodeManager::toType(const string &typeString) const {
+    if (_typeMapper.contains(typeString)) {
+        return _typeMapper.at(typeString);
+    } else {
+        throw std::runtime_error("Invalid node type");
+    }
+}
+
+void NodeManager::updateNode(NodeServer &&nodeServer) {
+    unique_lock<shared_mutex> lock(_sharedMutex);
+    auto[nodePair, isNew] = _allNodes[nodeServer.getType()].try_emplace(
+            nodeServer.getNetEndian(),
+            move(nodeServer)
+    );
+    if (!isNew) {
+        nodePair->second.setInfo(move(nodeServer.getInfo()));
+    }
+    nodePair->second.updateLastSeen();
+    updateTimer(nodePair->second);
+}
+
+string NodeManager::getBestNode(const NodeServer::Type &type) const {
+    //TODO: implement logics
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    return _allNodes.at(type).begin()->second.getIpPort();
+}
+
+Json::Value NodeManager::parseInfo() const {
+    Json::Value result(Json::arrayValue);
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    for (const auto&[type, nodeList]: _allNodes) {
+        Json::Value tempType;
+        tempType["typeId"] = static_cast<int>(type);
+        tempType["nodeList"] = Json::arrayValue;
+        for (const auto &[_, node]: nodeList) {
+            tempType["nodeList"].append(node.parseNode());
+        }
+        result.append(tempType);
+    }
+    return result;
+}
+
+void NodeManager::updateTimer(NodeServer &nodeServer) {
+    if (_waitTimes == 0) {
+        // Disable heartbeat timeout
+        return;
+    }
+    // Invalidate old heartbeat timeout
+    app().getLoop()->invalidateTimer(nodeServer.getTimerId());
+
+    const auto &type = nodeServer.getType();
+    const auto &netEndian = nodeServer.getNetEndian();
+    // Set new heartbeat timeout
+    nodeServer.setTimerId(app().getLoop()->runAfter(
+            nodeServer.getInterval() * _waitTimes,
+            [this, type, netEndian]() {
+                unordered_map<NetEndian, NodeServer>::node_type node; // Declare node first to avoid memory leak
+                unique_lock<shared_mutex> lock(_sharedMutex);
+                node = _allNodes[type].extract(netEndian);
+                if (node.empty()) {
+                    LOG_WARN << "Node " << netEndian << " already removed";
+                }
+            }
+    ));
+}
---
+++ Connect/plugins/NodeManager.h
@@ -0,0 +1,43 @@
+//
+// Created by Particle_G on 2021/8/19.
+//
+
+#pragma once
+
+#include <drogon/plugins/Plugin.h>
+#include <list>
+#include <structures/NodeServer.h>
+#include <shared_mutex>
+
+namespace tech::plugins {
+    class NodeManager : public drogon::Plugin<NodeManager> {
+    public:
+        using NetEndian = uint64_t;
+
+        NodeManager() = default;
+
+        void initAndStart(const Json::Value &config) override;
+
+        void shutdown() override;
+
+        structures::NodeServer::Type toType(const std::string &typeString) const;
+
+        void updateNode(structures::NodeServer &&nodeServer);
+
+        std::string getBestNode(const structures::NodeServer::Type &type) const;
+
+        Json::Value parseInfo() const;
+
+    private:
+        mutable std::shared_mutex _sharedMutex;
+
+        std::atomic<uint32_t> _waitTimes;
+        std::unordered_map<std::string, structures::NodeServer::Type> _typeMapper;
+        std::unordered_map<
+                structures::NodeServer::Type,
+                std::unordered_map<NetEndian, structures::NodeServer>
+        > _allNodes;
+
+        void updateTimer(structures::NodeServer &nodeServer);
+    };
+}
