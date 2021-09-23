2d8fc43f1bf5bf7c199d4d19d245fbd4a3ce29c4
ParticleG <particle_g@outlook.com>
2021-09-23 09:18:21 +0800

    - Modify main.cc to link shared libraries

--- User/main.cc
+++ User/main.cc
@@ -1,5 +1,9 @@
 #include <drogon/drogon.h>
 
+// TODO: Think another way to trick the linker to link these headers
+#include <plugins/Authorizer.h>
+#include <plugins/Perfmon.h>
+
 using namespace drogon;
 
 int main() {
