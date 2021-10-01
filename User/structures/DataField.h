f56da1f8b6ae1b8afb0e66b6aa92983e3ed19911
ParticleG <particle_g@outlook.com>
2021-10-02 06:13:53 +0800

    - Add enum class DataField

---
+++ User/structures/DataField.h
@@ -0,0 +1,13 @@
+//
+// Created by particleg on 2021/10/2.
+//
+
+#pragma once
+
+namespace tech::structures {
+    enum class DataField {
+        kPublic,
+        kProtected,
+        kPrivate
+    };
+}
