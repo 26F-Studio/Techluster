6333045ac2dbc42e4d7c3651ae0be7e050ab8cea
ParticleG <particle_g@outlook.com>
2021-10-11 22:04:49 +0800

    - Implement some basic methods

--- Gaming/plugins/RoomManager.cc
+++ Gaming/plugins/RoomManager.cc
@@ -4,14 +4,29 @@
 
 #include <drogon/drogon.h>
 #include <plugins/RoomManager.h>
+#include <strategies/Action.h>
+#include <structures/Exceptions.h>
+#include <structures/MessageHandler.h>
+#include <structures/Player.h>
 #include <utils/crypto.h>
+#include <utils/serializer.h>
 
 using namespace drogon;
 using namespace std;
-using namespace sw::redis;
 using namespace tech::plugins;
+using namespace tech::strategies;
 using namespace tech::structures;
 using namespace tech::utils;
+
+RoomManager::SharedRoom::SharedRoom(
+        Room &room,
+        shared_lock<shared_mutex> &&lock
+) : room(room), _lock(move(lock)) {}
+
+RoomManager::UniqueRoom::UniqueRoom(
+        Room &room,
+        unique_lock<shared_mutex> &&lock
+) : room(room), _lock(move(lock)) {}
 
 void RoomManager::initAndStart(const Json::Value &config) {
     LOG_INFO << "RoomManager loaded.";
@@ -20,20 +35,139 @@ void RoomManager::initAndStart(const Json::Value &config) {
 void RoomManager::shutdown() { LOG_INFO << "RoomManager shutdown."; }
 
 string RoomManager::createRoom(
-        const uint32_t &capacity,
         const string &password,
+        const uint32_t &capacity,
         const Json::Value &info,
-        const Json::Value &config
+        const Json::Value &data
 ) {
-    return _redisHelper->createRoom(
+    auto room = Room(
+            password,
             capacity,
-            crypto::blake2b(password),
             info,
-            config
+            data
     );
+    auto roomId = room.roomId();
+    {
+        unique_lock<shared_mutex> lock(_sharedMutex);
+        auto[_, result]=_roomMap.try_emplace(
+                roomId,
+                RoomWithMutex(move(room))
+        );
+        if (!result) {
+            LOG_FATAL << "UUID collided!";
+            return {};
+        }
+    }
+    return roomId;
 }
 
-void RoomManager::joinRoom(const string &roomId) {
-
+void RoomManager::removeRoom(const string &roomId) {
+    unique_lock<std::shared_mutex> lock(_sharedMutex);
+    auto iter = _roomMap.find(roomId);
+    if (iter != _roomMap.end()) {
+        typename decltype(_roomMap)::node_type node; // Declare node first to avoid memory leak
+        unique_lock<shared_mutex> innerLock(*iter->second._sharedMutex);
+        node = _roomMap.extract(roomId);
+        if (node.empty()) {
+            LOG_INFO << "Room " << roomId << " already removed";
+        }
+    } else {
+        LOG_INFO << "Room " << roomId << " does not exist";
+    }
 }
 
+RoomManager::SharedRoom RoomManager::getSharedRoom(const string &roomId) {
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    auto iter = _roomMap.find(roomId);
+    if (iter != _roomMap.end()) {
+        return SharedRoom(
+                iter->second._room,
+                move(shared_lock<shared_mutex>(
+                        *iter->second._sharedMutex
+                ))
+        );
+    }
+    throw RoomException::RoomNotFound("Invalid room id");
+}
+
+RoomManager::UniqueRoom RoomManager::getUniqueRoom(const string &roomId) {
+    shared_lock<shared_mutex> lock(_sharedMutex);
+    auto iter = _roomMap.find(roomId);
+    if (iter != _roomMap.end()) {
+        return UniqueRoom(
+                iter->second._room,
+                move(unique_lock<shared_mutex>(
+                        *iter->second._sharedMutex
+                ))
+        );
+    }
+    throw RoomException::RoomNotFound("Invalid room id");
+}
+
+void RoomManager::joinRoom(
+        const string &roomId,
+        const string &password,
+        const WebSocketConnectionPtr &connection
+) {
+    auto &room = getSharedRoom(roomId).room;
+    if (!room.checkPassword(password)) {
+        throw RoomException::InvalidPassword("Password is incorrect");
+    }
+
+    const auto &player = connection->getContext<Player>();
+
+    room.subscribe(connection);
+
+    Json::Value message, response;
+    message["origin"] = static_cast<int>(Origin::other);
+    message["action"] = toUInt(Action::roomJoin);
+    message["data"] = player->info();
+    room.publish(
+            move(serializer::json::stringify(message)),
+            player->userId()
+    );
+
+    response["type"] = static_cast<int>(Origin::self);
+    response["action"] = toUInt(Action::roomJoin);
+    response["data"] = room.parse();
+    connection->send(serializer::json::stringify(response));
+}
+
+void RoomManager::leaveRoom(
+        const WebSocketConnectionPtr &connection
+) {
+    if (connection->connected()) {
+        Json::Value response;
+        response["type"] = static_cast<int>(Origin::self);
+        response["action"] = static_cast<int>(Action::roomLeave);
+        connection->send(serializer::json::stringify(response));
+    }
+
+    const auto &player = connection->getContext<Player>();
+    auto roomId = player->getJoinedId();
+    bool isEmpty;
+    {
+        auto &room = getSharedRoom(roomId).room;
+        if (room.unsubscribe(connection)) {
+            Json::Value message;
+            message["type"] = static_cast<int>(Origin::other);
+            message["action"] = static_cast<int>(Action::roomLeave);
+            message["data"] = player->info();
+            room.publish(move(serializer::json::stringify(message)));
+        }
+        isEmpty = room.empty();
+    }
+    if (isEmpty) { // TODO: Check if thread safe
+        removeRoom(roomId);
+    }
+}
+
+RoomManager::RoomWithMutex::RoomWithMutex(
+        Room &&room
+) : _room(move(room)),
+    _sharedMutex(new shared_mutex()) {}
+
+RoomManager::RoomWithMutex::RoomWithMutex(
+        RoomManager::RoomWithMutex &&moved
+) noexcept: _room(move(moved._room)),
+            _sharedMutex(move(moved._sharedMutex)) {}
--- Gaming/plugins/RoomManager.h
+++ Gaming/plugins/RoomManager.h
@@ -5,28 +5,76 @@
 #pragma once
 
 #include <drogon/plugins/Plugin.h>
-#include <structures/RedisHelper.h>
+#include <structures/Room.h>
 
 namespace tech::plugins {
     class RoomManager : public drogon::Plugin<RoomManager> {
     public:
+        class SharedRoom {
+        public:
+            explicit SharedRoom(
+                    structures::Room &room,
+                    std::shared_lock<std::shared_mutex> &&lock
+            );
+
+            structures::Room &room;
+        private:
+            std::shared_lock<std::shared_mutex> _lock;
+        };
+
+        class UniqueRoom {
+        public:
+            explicit UniqueRoom(
+                    structures::Room &room,
+                    std::unique_lock<std::shared_mutex> &&lock
+            );
+
+            structures::Room &room;
+        private:
+            std::unique_lock<std::shared_mutex> _lock;
+        };
+
         RoomManager() = default;
 
         void initAndStart(const Json::Value &config) override;
 
         void shutdown() override;
 
-        std::string createRoom(
-                const uint32_t &capacity,
+        [[nodiscard]] std::string createRoom(
                 const std::string &password,
+                const uint32_t &capacity,
                 const Json::Value &info,
-                const Json::Value &config
+                const Json::Value &data
         );
 
-        void joinRoom(const std::string &roomId);
+        void removeRoom(const std::string &roomId);
+
+        SharedRoom getSharedRoom(const std::string &roomId);
+
+        UniqueRoom getUniqueRoom(const std::string &roomId);
+
+        void joinRoom(
+                const std::string &roomId,
+                const std::string &password,
+                const drogon::WebSocketConnectionPtr &connection
+        );
+
+        void leaveRoom(
+                const drogon::WebSocketConnectionPtr &connection
+        );
 
     private:
-        std::unique_ptr<tech::structures::RedisHelper> _redisHelper;
+        struct RoomWithMutex {
+            explicit RoomWithMutex(structures::Room &&room);
+
+            RoomWithMutex(RoomWithMutex &&moved) noexcept;
+
+            structures::Room _room;
+            mutable std::unique_ptr<std::shared_mutex> _sharedMutex; // TODO: Try not using unique_ptr
+        };
+
+        mutable std::shared_mutex _sharedMutex;
+        std::unordered_map<std::string, RoomWithMutex> _roomMap;
     };
 }
 
