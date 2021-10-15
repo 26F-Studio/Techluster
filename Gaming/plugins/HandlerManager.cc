157a9345e68cff7dc2b092864624163cbb0f74b8
ParticleG <particle_g@outlook.com>
2021-10-15 09:03:06 +0800

    - Implement HandlerManager

--- Gaming/plugins/HandlerManager.cc
+++ Gaming/plugins/HandlerManager.cc
@@ -4,6 +4,17 @@
 
 #include <plugins/HandlerManager.h>
 #include <strategies/Action.h>
+#include <strategies/PlayerConfig.h>
+#include <strategies/PlayerGroup.h>
+#include <strategies/PlayerRole.h>
+#include <strategies/PlayerState.h>
+#include <strategies/PlayerType.h>
+#include <strategies/RoomCreate.h>
+#include <strategies/RoomJoin.h>
+#include <strategies/RoomKick.h>
+#include <strategies/RoomLeave.h>
+#include <strategies/RoomList.h>
+#include <strategies/RoomRemove.h>
 
 using namespace tech::plugins;
 using namespace tech::strategies;
@@ -13,7 +24,17 @@ using namespace drogon;
 using namespace std;
 
 void HandlerManager::initAndStart(const Json::Value &config) {
-//    _handlerFactory.registerHandler<GetRoomList>(toUInt(Action::getRoomList));
+    _handlerFactory.registerHandler<PlayerConfig>(toUInt(Action::roomCreate));
+    _handlerFactory.registerHandler<PlayerGroup>(toUInt(Action::roomJoin));
+    _handlerFactory.registerHandler<PlayerRole>(toUInt(Action::roomKick));
+    _handlerFactory.registerHandler<PlayerState>(toUInt(Action::roomLeave));
+    _handlerFactory.registerHandler<PlayerType>(toUInt(Action::roomList));
+    _handlerFactory.registerHandler<RoomCreate>(toUInt(Action::roomRemove));
+    _handlerFactory.registerHandler<RoomJoin>(toUInt(Action::playerConfig));
+    _handlerFactory.registerHandler<RoomKick>(toUInt(Action::playerGroup));
+    _handlerFactory.registerHandler<RoomLeave>(toUInt(Action::playerRole));
+    _handlerFactory.registerHandler<RoomList>(toUInt(Action::playerState));
+    _handlerFactory.registerHandler<RoomRemove>(toUInt(Action::playerType));
 
     LOG_INFO << "HandlerManager loaded.";
 }
