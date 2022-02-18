1e46d8fdcd3e3b9eb80c59eba3c0e756754e337b
ParticleG <particle_g@outlook.com>
2022-02-18 23:36:39 +0800

    - Moved from structure class NodeServer

---
+++ Shared/types/NodeType.h
@@ -0,0 +1,16 @@
+//
+// Created by ParticleG on 2022/2/9.
+//
+
+#pragma once
+
+namespace tech::types {
+    enum class NodeType {
+        all,
+        connect,
+        gaming,
+        message,
+        transfer,
+        user
+    };
+}
