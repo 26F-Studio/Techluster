d1b1dfff1d84fa3d56c8d9563e92fc14d163b4df
ParticleG <particle_g@outlook.com>
2022-02-18 23:25:05 +0800

    - Add shared filter headers

--- Connect/main.cc
+++ Connect/main.cc
@@ -1,5 +1,9 @@
 #include <drogon/drogon.h>
 
+// TODO: Think another way to trick the linker to link these headers
+#include <filters/CheckCredential.h>
+#include <filters/CheckNodeType.h>
+
 using namespace drogon;
 
 int main() {
