7654f15424745378a023c7e0b3f861662e54c3d8
ParticleG <particle_g@outlook.com>
2022-02-19 00:49:32 +0800

    - Use I18NHelper to handle error messages - Use shared filters - Use custom exception handlers - Use custom json handlers - Merge service's functionality

--- User/controllers/Auth.cc
+++ User/controllers/Auth.cc
@@ -3,129 +3,124 @@
 //
 
 #include <controllers/Auth.h>
+#include <helpers/RequestJson.h>
+#include <helpers/ResponseJson.h>
+#include <structures/Exceptions.h>
+#include <utils/http.h>
 
 using namespace drogon;
 using namespace std;
 using namespace tech::api::v2;
+using namespace tech::helpers;
+using namespace tech::plugins;
+using namespace tech::structures;
+using namespace tech::types;
 using namespace tech::utils;
 
+Auth::Auth() :
+        ResponseJsonHandler(
+                [](const ResponseException &e, ResponseJson &response, HttpStatusCode &code) {
+                    code = e.statusCode();
+                    // TODO: Check if this causes too much copying
+                    response(e.toJson());
+                },
+                [this](const orm::DrogonDbException &e, ResponseJson &response, HttpStatusCode &code) {
+                    LOG_ERROR << e.base().what();
+                    code = k500InternalServerError;
+                    response.setResult(ResultCode::databaseError);
+                    response.setMessage(i18n("databaseError"));
+                },
+                [this](const exception &e, ResponseJson &response, HttpStatusCode &code) {
+                    LOG_ERROR << e.what();
+                    code = drogon::k500InternalServerError;
+                    response.setResult(ResultCode::internalError);
+                    response.setMessage(i18n("internalError"));
+                    response.setReason(e);
+                }
+        ),
+        I18nHelper(CMAKE_PROJECT_NAME),
+        _dataManager(app().getPlugin<DataManager>()) {}
+
 void Auth::check(const HttpRequestPtr &req, function<void(const HttpResponsePtr &)> &&callback) {
-    HttpStatusCode code = HttpStatusCode::k200OK;
-    Json::Value response;
-    auto accessToken = req->getHeader("x-access-token");
-    if (accessToken.empty()) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Invalid x-access-token header";
-        http::fromJson(code, response, callback);
-        return;
-    }
-    http::fromJson(code, _service.check(code, accessToken), callback);
+    HttpStatusCode code = k200OK;
+    ResponseJson response;
+    handleExceptions([&]() {
+        response.setData(_dataManager->getUserId(
+                req->attributes()->get<string>("accessToken")
+        ));
+    }, response, code);
+    http::fromJson(code, response.ref(), callback);
 }
 
 void Auth::refresh(const HttpRequestPtr &req, function<void(const HttpResponsePtr &)> &&callback) {
-    HttpStatusCode code = HttpStatusCode::k200OK;
-    Json::Value response;
-    auto refreshToken = req->getHeader("x-refresh-token");
-    if (refreshToken.empty()) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Invalid x-refresh-token header";
-        http::fromJson(code, response, callback);
-        return;
-    }
-    http::fromJson(code, _service.refresh(code, refreshToken), callback);
+    HttpStatusCode code = k200OK;
+    ResponseJson response;
+    handleExceptions([&]() {
+        response.setData(move(_dataManager->refresh(
+                req->attributes()->get<string>("refreshToken")
+        ).parse()));
+    }, response, code);
+    http::fromJson(code, response.ref(), callback);
 }
 
 void Auth::verifyEmail(const HttpRequestPtr &req, function<void(const HttpResponsePtr &)> &&callback) {
-    HttpStatusCode code = HttpStatusCode::k200OK;
-    http::fromJson(code, _service.verifyEmail(
-            code,
-            req->attributes()->get<Json::Value>("data")
-    ), callback);
+    HttpStatusCode code = k200OK;
+    ResponseJson response;
+    handleExceptions([&]() {
+        _dataManager->verifyEmail(
+                req->attributes()->get<RequestJson>("requestJson")["email"].asString()
+        );
+    }, response, code);
+    http::fromJson(code, response.ref(), callback);
 }
 
 void Auth::loginEmail(const HttpRequestPtr &req, function<void(const HttpResponsePtr &)> &&callback) {
-    HttpStatusCode code = HttpStatusCode::k200OK;
-    Json::Value request, response;
-    string parseError = http::toJson(req, request);
-    if (!parseError.empty()) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Wrong format: " + parseError;
-        http::fromJson(code, response, callback);
-        return;
-    }
-    if (!(
-            request.isMember("email") && request["email"].isString() &&
-            (
-                    (request.isMember("code") && request["code"].isString()) ||
-                    (request.isMember("password") && request["password"].isString())
-            )
-    )) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Invalid parameters";
-        http::fromJson(code, response, callback);
-        return;
-    }
-    http::fromJson(code, _service.loginMail(code, request), callback);
+    HttpStatusCode code = k200OK;
+    ResponseJson response;
+    handleExceptions([&]() {
+        auto request = req->attributes()->get<RequestJson>("requestJson");
+        if (request.check("code", JsonValue::String)) {
+            const auto &[tokens, isNew] = _dataManager->loginEmailCode(
+                    request["email"].asString(),
+                    request["code"].asString()
+            );
+            if (isNew) { response.setResult(ResultCode::continued); }
+            response.setData(move(tokens.parse()));
+        } else {
+            const auto &tokens = _dataManager->loginEmailPassword(
+                    request["email"].asString(),
+                    request["password"].asString()
+            );
+            response.setData(move(tokens.parse()));
+        }
+    }, response, code);
+    http::fromJson(code, response.ref(), callback);
 }
 
 void Auth::resetEmail(const HttpRequestPtr &req, function<void(const HttpResponsePtr &)> &&callback) {
-    HttpStatusCode code = HttpStatusCode::k200OK;
-    Json::Value request, response;
-    string parseError = http::toJson(req, request);
-    if (!parseError.empty()) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Wrong format: " + parseError;
-        http::fromJson(code, response, callback);
-        return;
-    }
-    if (!(
-            request.isMember("email") && request["email"].isString() &&
-            request.isMember("code") && request["code"].isString() &&
-            request.isMember("newPassword") && request["newPassword"].isString()
-    )) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Invalid parameters";
-        http::fromJson(code, response, callback);
-        return;
-    }
-    http::fromJson(code, _service.resetEmail(code, request), callback);
+    HttpStatusCode code = k200OK;
+    ResponseJson response;
+    handleExceptions([&]() {
+        auto request = req->attributes()->get<RequestJson>("requestJson");
+        _dataManager->resetEmail(
+                request["email"].asString(),
+                request["code"].asString(),
+                request["newPassword"].asString()
+        );
+    }, response, code);
+    http::fromJson(code, response.ref(), callback);
 }
 
 void Auth::migrateEmail(const HttpRequestPtr &req, function<void(const HttpResponsePtr &)> &&callback) {
-    HttpStatusCode code = HttpStatusCode::k200OK;
-    Json::Value request, response;
-    auto accessToken = req->getHeader("x-access-token");
-    if (accessToken.empty()) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Invalid x-access-token header";
-        http::fromJson(code, response, callback);
-        return;
-    }
-    string parseError = http::toJson(req, request);
-    if (!parseError.empty()) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Wrong format: " + parseError;
-        http::fromJson(code, response, callback);
-        return;
-    }
-    if (!(
-            request.isMember("newEmail") && request["newEmail"].isString() &&
-            request.isMember("code") && request["code"].isString()
-    )) {
-        code = drogon::k400BadRequest;
-        response["type"] = "Error";
-        response["reason"] = "Invalid parameters";
-        http::fromJson(code, response, callback);
-        return;
-    }
-    request["accessToken"] = accessToken;
-    http::fromJson(code, _service.migrateEmail(code, request), callback);
+    HttpStatusCode code = k200OK;
+    ResponseJson response;
+    handleExceptions([&]() {
+        auto request = req->attributes()->get<RequestJson>("requestJson");
+        _dataManager->migrateEmail(
+                req->attributes()->get<string>("accessToken"),
+                request["newEmail"].asString(),
+                request["code"].asString()
+        );
+    }, response, code);
+    http::fromJson(code, response.ref(), callback);
 }
--- User/controllers/Auth.h
+++ User/controllers/Auth.h
@@ -5,18 +5,36 @@
 #pragma once
 
 #include <drogon/HttpController.h>
-#include <services/Auth.h>
+#include <plugins/DataManager.h>
+#include <structures/ExceptionHandlers.h>
 
 namespace tech::api::v2 {
-    class Auth : public drogon::HttpController<Auth> {
+    class Auth :
+            public drogon::HttpController<Auth>,
+            public structures::ResponseJsonHandler,
+            public helpers::I18nHelper<Auth> {
     public:
+        Auth();
+
         METHOD_LIST_BEGIN
-            METHOD_ADD(Auth::check, "/check", drogon::Get);
-            METHOD_ADD(Auth::refresh, "/refresh", drogon::Get);
-            METHOD_ADD(Auth::verifyEmail, "/verify/email", drogon::Post, "tech::filters::EmailCoolDown", "tech::filters::IpCoolDown");
-            METHOD_ADD(Auth::loginEmail, "/login/email", drogon::Post);
-            METHOD_ADD(Auth::resetEmail, "/reset/email", drogon::Put);
-            METHOD_ADD(Auth::migrateEmail, "/migrate/email", drogon::Put);
+            METHOD_ADD(Auth::check, "/check", drogon::Get, "tech::filters::CheckAccessToken");
+            METHOD_ADD(Auth::refresh, "/refresh", drogon::Get, "tech::filters::CheckRefreshToken");
+            METHOD_ADD(
+                    Auth::verifyEmail,
+                    "/verify/email",
+                    drogon::Post,
+                    "tech::filters::AuthVerifyEmail",
+                    "tech::filters::EmailThreshold",
+                    "tech::filters::IpThreshold"
+            );
+            METHOD_ADD(Auth::loginEmail, "/login/email", drogon::Post, "tech::filters::AuthLoginEmail");
+            METHOD_ADD(Auth::resetEmail, "/reset/email", drogon::Put, "tech::filters::AuthResetEmail");
+            METHOD_ADD(
+                    Auth::migrateEmail,
+                    "/migrate/email",
+                    drogon::Put,
+                    "tech::filters::CheckAccessToken",
+                    "tech::filters::AuthMigrateEmail");
         METHOD_LIST_END
 
         void check(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr &)> &&callback);
@@ -32,6 +50,6 @@ namespace tech::api::v2 {
         void migrateEmail(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr &)> &&callback);
 
     private:
-        services::Auth _service;
+        plugins::DataManager *_dataManager;
     };
 }
