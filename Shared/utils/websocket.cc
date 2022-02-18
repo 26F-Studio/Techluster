4aaf67135e5cbbbe9422c81b5f6ae3678630db17
ParticleG <particle_g@outlook.com>
2022-02-19 00:20:32 +0800

    - Use custom Json helpers

--- Shared/utils/websocket.cc
+++ Shared/utils/websocket.cc
@@ -2,10 +2,10 @@
 // Created by Parti on 2021/2/5.
 //
 
-#include <structures/JsonHelper.h>
+#include <helpers/BasicJson.h>
 #include <utils/websocket.h>
 
-using namespace tech::structures;
+using namespace tech::helpers;
 using namespace tech::utils;
 using namespace drogon;
 using namespace std;
@@ -15,6 +15,6 @@ void websocket::initPing(
         const Json::Value &initMessage,
         const chrono::duration<long double> &interval
 ) {
-    wsConnPtr->send(JsonHelper(initMessage).stringify());
+    wsConnPtr->send(BasicJson(initMessage).stringify());
     wsConnPtr->setPingMessage("", interval);
 }
