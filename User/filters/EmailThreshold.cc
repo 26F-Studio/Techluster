3a86fefad734be400561e7ea32b00c07e2bf1369
ParticleG <particle_g@outlook.com>
2022-02-19 00:58:54 +0800

    - Refactor to EmailThreshold - Use custom json helpers - Add filter description

---
+++ User/filters/EmailThreshold.cc
@@ -0,0 +1,46 @@
+//
+// Created by particleg on 2021/9/27.
+//
+
+#include <filters/EmailThreshold.h>
+#include <helpers/RequestJson.h>
+#include <helpers/ResponseJson.h>
+#include <plugins/DataManager.h>
+#include <structures/Exceptions.h>
+#include <utils/http.h>
+
+using namespace drogon;
+using namespace std;
+using namespace tech::filters;
+using namespace tech::helpers;
+using namespace tech::plugins;
+using namespace tech::structures;
+using namespace tech::types;
+using namespace tech::utils;
+
+EmailThreshold::EmailThreshold() : I18nHelper(CMAKE_PROJECT_NAME) {}
+
+void tech::filters::EmailThreshold::doFilter(
+        const HttpRequestPtr &req,
+        FilterCallback &&failedCb,
+        FilterChainCallback &&nextCb
+) {
+    auto requestJson = req->attributes()->get<RequestJson>("requestJson");
+    try {
+        if (!app().getPlugin<DataManager>()->emailLimit(requestJson["email"].asString())) {
+            ResponseJson response;
+            response.setResult(ResultCode::tooFrequent);
+            response.setMessage(i18n("tooFrequent.email"));
+            http::fromJson(k429TooManyRequests, response.ref(), failedCb);
+            return;
+        }
+    } catch (const exception &e) {
+        LOG_ERROR << e.what();
+        ResponseJson response;
+        response.setResult(ResultCode::internalError);
+        response.setMessage(i18n("internalError"));
+        http::fromJson(k500InternalServerError, response.ref(), failedCb);
+        return;
+    }
+    nextCb();
+}
---
+++ User/filters/EmailThreshold.h
@@ -0,0 +1,27 @@
+//
+// Created by particleg on 2021/9/27.
+//
+
+#pragma once
+
+#include <drogon/HttpFilter.h>
+#include <helpers/I18nHelper.h>
+
+/**
+ * @brief This filter checks request frequency from specific email address
+ * @param email: String
+ */
+namespace tech::filters {
+    class EmailThreshold :
+            public drogon::HttpFilter<EmailThreshold>,
+            public helpers::I18nHelper<EmailThreshold> {
+    public:
+        EmailThreshold();
+
+        void doFilter(
+                const drogon::HttpRequestPtr &req,
+                drogon::FilterCallback &&failedCb,
+                drogon::FilterChainCallback &&nextCb
+        ) override;
+    };
+}
