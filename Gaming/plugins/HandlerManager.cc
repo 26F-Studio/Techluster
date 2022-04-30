3cdacf4d3710d20f50d8fef9262ab16579e2cd75
ParticleG <particle_g@outlook.com>
2022-04-30 14:42:47 +0800

    - Use `magic_enum` instead of custom `toInt`

--- Gaming/plugins/HandlerManager.cc
+++ Gaming/plugins/HandlerManager.cc
@@ -2,8 +2,8 @@
 // Created by Parti on 2021/2/19.
 //
 
+#include <magic_enum.hpp>
 #include <plugins/HandlerManager.h>
-#include <strategies/Action.h>
 #include <strategies/PlayerConfig.h>
 #include <strategies/PlayerGroup.h>
 #include <strategies/PlayerPing.h>
@@ -17,31 +17,36 @@
 #include <strategies/RoomKick.h>
 #include <strategies/RoomLeave.h>
 #include <strategies/RoomList.h>
+#include <strategies/RoomPassword.h>
 #include <strategies/RoomRemove.h>
+#include <types/Action.h>
 
+using namespace drogon;
+using namespace magic_enum;
+using namespace std;
 using namespace tech::plugins;
 using namespace tech::strategies;
 using namespace tech::structures;
+using namespace tech::types;
 using namespace tech::utils;
-using namespace drogon;
-using namespace std;
 
 void HandlerManager::initAndStart(const Json::Value &config) {
-    _handlerFactory.registerHandler<PlayerConfig>(toUInt(Action::playerConfig));
-    _handlerFactory.registerHandler<PlayerGroup>(toUInt(Action::playerGroup));
-    _handlerFactory.registerHandler<PlayerPing>(toUInt(Action::playerPing));
-    _handlerFactory.registerHandler<PlayerRole>(toUInt(Action::playerRole));
-    _handlerFactory.registerHandler<PlayerState>(toUInt(Action::playerState));
-    _handlerFactory.registerHandler<PlayerType>(toUInt(Action::playerType));
+    _handlerFactory.registerHandler<PlayerConfig>(enum_integer(Action::playerConfig));
+    _handlerFactory.registerHandler<PlayerGroup>(enum_integer(Action::playerGroup));
+    _handlerFactory.registerHandler<PlayerPing>(enum_integer(Action::playerPing));
+    _handlerFactory.registerHandler<PlayerRole>(enum_integer(Action::playerRole));
+    _handlerFactory.registerHandler<PlayerState>(enum_integer(Action::playerState));
+    _handlerFactory.registerHandler<PlayerType>(enum_integer(Action::playerType));
 
-    _handlerFactory.registerHandler<RoomCreate>(toUInt(Action::roomCreate));
-    _handlerFactory.registerHandler<RoomData>(toUInt(Action::roomData));
-    _handlerFactory.registerHandler<RoomInfo>(toUInt(Action::roomInfo));
-    _handlerFactory.registerHandler<RoomJoin>(toUInt(Action::roomJoin));
-    _handlerFactory.registerHandler<RoomKick>(toUInt(Action::roomKick));
-    _handlerFactory.registerHandler<RoomLeave>(toUInt(Action::roomLeave));
-    _handlerFactory.registerHandler<RoomList>(toUInt(Action::roomList));
-    _handlerFactory.registerHandler<RoomRemove>(toUInt(Action::roomRemove));
+    _handlerFactory.registerHandler<RoomCreate>(enum_integer(Action::roomCreate));
+    _handlerFactory.registerHandler<RoomData>(enum_integer(Action::roomData));
+    _handlerFactory.registerHandler<RoomInfo>(enum_integer(Action::roomInfo));
+    _handlerFactory.registerHandler<RoomJoin>(enum_integer(Action::roomJoin));
+    _handlerFactory.registerHandler<RoomKick>(enum_integer(Action::roomKick));
+    _handlerFactory.registerHandler<RoomLeave>(enum_integer(Action::roomLeave));
+    _handlerFactory.registerHandler<RoomList>(enum_integer(Action::roomList));
+    _handlerFactory.registerHandler<RoomPassword>(enum_integer(Action::roomPassword));
+    _handlerFactory.registerHandler<RoomRemove>(enum_integer(Action::roomRemove));
 
     LOG_INFO << "HandlerManager loaded.";
 }
