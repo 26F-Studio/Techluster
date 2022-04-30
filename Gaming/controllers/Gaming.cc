a9beb0bbe69c196a4d66a7b10acc89bfa4d61f27
ParticleG <particle_g@outlook.com>
2022-04-30 14:43:34 +0800

    - Minor changes for better i18n integration

--- Gaming/controllers/Gaming.cc
+++ Gaming/controllers/Gaming.cc
@@ -3,33 +3,47 @@
 //
 
 #include <controllers/Gaming.h>
-#include <plugins/RoomManager.h>
+#include <magic_enum.hpp>
 #include <structures/Player.h>
+#include <types/Action.h>
 
 using namespace drogon;
+using namespace magic_enum;
 using namespace std;
+using namespace tech::helpers;
 using namespace tech::plugins;
+using namespace tech::structures;
 using namespace tech::ws::v2;
-using namespace tech::utils;
+using namespace tech::types;
+
+Gaming::Gaming() :
+        _connectionManager(app().getPlugin<ConnectionManager>()),
+        _roomManager(app().getPlugin<RoomManager>()) {}
 
 void Gaming::handleNewConnection(
         const HttpRequestPtr &req,
         const WebSocketConnectionPtr &wsConnPtr
 ) {
     auto id = req->getAttributes()->get<int64_t>("id");
-    wsConnPtr->setContext(make_shared<structures::Player>(id));
-
-    Json::Value initMessage;
-    initMessage["type"] = "Connect";
-    websocket::initPing(wsConnPtr, initMessage, chrono::seconds(10));
+    wsConnPtr->setContext(make_shared<Player>(id));
+    wsConnPtr->setPingMessage("", chrono::seconds(10));
+    _connectionManager->subscribe(wsConnPtr);
 }
 
 void Gaming::handleConnectionClosed(const WebSocketConnectionPtr &wsConnPtr) {
-    if (wsConnPtr->hasContext()) {
-        try {
-            app().getPlugin<RoomManager>()->leaveRoom(wsConnPtr);
-        } catch (const internal::BaseException &error) {
-            LOG_DEBUG << "Closed without unsubscription: " << error.what();
+    const auto &player = wsConnPtr->getContext<Player>();
+    if (player) {
+        if (!player->getRoomId().empty()) {
+            try {
+                _roomManager->roomLeave(enum_integer(Action::roomLeave), wsConnPtr);
+            } catch (const internal::BaseException &e) {
+                LOG_DEBUG << "Closed without leaving room: " << e.what();
+            }
         }
+        _connectionManager->unsubscribe(wsConnPtr);
     }
+}
+
+std::string Gaming::reason(const string &param) const {
+    return i18n(param);
 }
